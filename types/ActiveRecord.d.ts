05a9310ae701e0d700a04fab4ed2d2fab838b590
Heiko Holland <heiko.holland@me.com>
2018-01-08 18:36:07 +0100

    package.json

--- types/ActiveRecord.d.ts
+++ types/ActiveRecord.d.ts
@@ -11,26 +11,26 @@ export declare abstract class ActiveRecord extends Model {
     protected static _identifier: string;
     protected static _tableName: string;
     protected static _queryClass: any;
+    private static _defaultConfig;
     private static _config;
     protected static _db: {
         [model: string]: any;
     };
     private static _initialized;
     constructor(values?: any);
-    static readonly db: {
-        [model: string]: any;
-    };
+    static readonly db: any;
     readonly db: any;
     protected static _dbInit(): Promise<boolean>;
     static initialized(model: string): void;
     static config: ActiveRecordConfig;
-    static init(): void;
+    static init(): Promise<boolean>;
     private _initRelations();
-    readonly class: any;
+    static addRelation(relation: ActiveRecordRelation): void;
     id: string | number;
     readonly isNewRecord: boolean;
     static find(): ActiveQuery & any;
     static findOne(condition?: any): Promise<ActiveRecord & any>;
     static findAll(condition?: {}): Promise<ActiveRecord[] & any[]>;
     abstract save(): Promise<this>;
+    static save(objects: any): Promise<any[]>;
 }
